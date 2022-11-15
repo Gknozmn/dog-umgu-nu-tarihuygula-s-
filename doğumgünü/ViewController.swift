b29676d0dc1cd74bea29db654448b9954f6344c1
GÖKHAN ÖZMEN <gkhanozmn@gmail.com>
2022-11-16 01:25:42 +0300

    Initial Commit

---
+++ doğumgünü/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  doğumgünü
+//
+//  Created by GÖKHAN ÖZMEN on 28.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var BURTHDAYLABEL: UILabel!
+    @IBOutlet weak var namelabel: UILabel!
+    @IBOutlet weak var bırtahdaytextfield: UITextField!
+    @IBOutlet weak var nametextfield: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func KAYITTIKLANDI(_ sender: Any) {
+    }
+    
+}
+
